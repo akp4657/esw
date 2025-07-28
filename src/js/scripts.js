1ed1a669b552604c31e924b5964817d4df33d379
Spine <igniteboost.net@gmail.com>
2025-07-28 13:04:10 -0400

    Vercel analytics

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,3 +3,7 @@ window.addEventListener('DOMContentLoaded', event => {
     const listHoursArray = document.body.querySelectorAll('.list-hours li');
     listHoursArray[new Date().getDay()].classList.add(('today'));
 })
+
+// Vercel Analytics
+import { inject } from '@vercel/analytics';
+inject();
